e8373aab6191e6007636b68761f3217cea020194
aashutosh227 <aashutoshsingh126@gmail.com>
2020-07-23 01:59:26 +0530

    Application Initial Setup Done

---
+++ main.py
@@ -0,0 +1,4 @@
+#This file will be load first when the application will start 
+#This file will redirect to __init__.py
+from application import app   #app is an object in the application/__init__.py
+
