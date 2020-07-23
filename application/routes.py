361bec73ad4826b7ee5c95d6291272e42f046650
aashutosh227 <aashutoshsingh126@gmail.com>
2020-07-24 02:38:50 +0530

    Added routes for navigation menu

--- application/routes.py
+++ application/routes.py
@@ -3,7 +3,22 @@
 from application import app
 from flask import render_template
 
+#The following decoraters specify the patterns for which the index function will be called
+#So it is called for "/", "index" and "home".
 @app.route('/')
 @app.route('/index')
+@app.route('/home')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", login=False)
+
+@app.route('/login')
+def login():
+    return render_template("login.html", login=False)
+
+@app.route('/register')
+def register():
+    return render_template("register.html", login=False)
+
+@app.route('/courses')
+def courses():
+    return render_template("course.html", login=False)
