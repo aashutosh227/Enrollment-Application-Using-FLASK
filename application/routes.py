cba76ab2267330f88d5637010e1bc4be96a0b87c
aashutosh227 <aashutoshsingh126@gmail.com>
2020-07-27 03:28:21 +0530

    Handling GET and POST requests from enroll form

--- application/routes.py
+++ application/routes.py
@@ -1,9 +1,15 @@
 #This file is created to seperately handle all routes in one file.
 
 from application import app
-from flask import render_template, jsonify
+from flask import render_template, jsonify, request, Response
 import json
 import os
+
+course_data = [{"courseID":"1111","title":"PHP 101","description":"Intro to PHP","credits":3,"term":"Fall, Spring"}, 
+                {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":4,"term":"Spring"}, 
+                {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":3,"term":"Fall"}, 
+                {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":3,"term":"Fall, Spring"}, 
+                {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":4,"term":"Fall"}]
 
 #The following decoraters specify the patterns for which the index function will be called
 #So it is called for "/", "index" and "home".
@@ -24,19 +30,22 @@ def register():
 @app.route('/classes/')
 @app.route('/classes/<term>')
 def classes(term="Spring 2019"):
-    course_data = [{"courseID":"1111","title":"PHP 101","description":"Intro to PHP","credits":3,"term":"Fall, Spring"}, 
-                {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":4,"term":"Spring"}, 
-                {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":3,"term":"Fall"}, 
-                {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":3,"term":"Fall, Spring"}, 
-                {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":4,"term":"Fall"}]
     return render_template("course.html",courseData = course_data, courses=True,term=term)
 
-# @app.route('/courses')
-# def courses():
-#     course_data = [{"courseID":"1111","title":"PHP 101","description":"Intro to PHP","credits":3,"term":"Fall, Spring"}, 
-#                 {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":4,"term":"Spring"}, 
-#                 {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":3,"term":"Fall"}, 
-#                 {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":3,"term":"Fall, Spring"}, 
-#                 {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":4,"term":"Fall"}]
-#     print(course_data)
-#     return render_template("course.html", courseData=course_data, courses=True)
+@app.route('/enrollment', methods = ["GET", "POST"])
+def enrollment():
+    id = request.form.get('courseID')
+    title = request.form.get('title')
+    term = request.form.get('term')
+    return render_template("enroll.html", enrollment=True, course = {"id": id, "title": title, "term": term})
+
+
+@app.route("/api/")
+@app.route("/api/<idx>")
+def api(idx=None):
+    if(idx==None):
+        jData = course_data
+    else:
+        jData = course_data[int(idx)]
+
+    return Response(json.dumps(jData), mimetype="application/json")
